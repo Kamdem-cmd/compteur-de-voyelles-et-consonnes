d2af2f082d2f0c035d7e926209453ed22208c59d
Kamdem-cmd <kamdemkevin87@gmail.com>
2025-01-04 18:26:14 +0100

    main.cpp
    Gestion des cas de majiscules...

--- main.cpp
+++ main.cpp
@@ -8,17 +8,25 @@ int main(int argc, char ** argv[]){
     // operations pour la recuperation de phrase
     std::cout << ">> " ;
     std::cin.getline(phrase, taille);
-    //std::getline(std::cin, phrase, '\0');
 
     char* ptr = phrase;
     while(*ptr != '\0'){
-        if(*ptr =='a'|| *ptr =='e'|| *ptr =='i'|| *ptr =='o'|| *ptr =='u'|| *ptr =='y') voyelles++; //incrementation du nbre de voyelles
-        if(*ptr =='b'|| *ptr =='c'|| *ptr =='d'|| *ptr =='z'|| *ptr =='f'|| *ptr =='g'||
-        *ptr =='h'|| *ptr =='j'|| *ptr =='k'|| *ptr =='l'|| *ptr =='m'|| *ptr =='n'||
-        *ptr =='p'|| *ptr =='q'|| *ptr =='r'|| *ptr =='s'|| *ptr =='t'|| *ptr =='v'|| 
-        *ptr =='w'|| *ptr =='x') consonnes++; //incrementation du nbre de consonnes
+        // incrementation du nombre de voyelles
+        if(*ptr =='a'|| *ptr =='e'|| *ptr =='i'|| *ptr =='o'|| *ptr =='u'|| *ptr =='y') voyelles++; // miniscules
+        if(*ptr =='a'-32|| *ptr =='e'-32|| *ptr =='i'-32|| *ptr =='o'-32|| *ptr =='u'-32|| *ptr =='y'-32)voyelles++; // majiscules
 
-        ptr++;
+        // incrementation du nombre de consonnes
+        if(*ptr =='b'|| *ptr =='c'|| *ptr =='d'|| *ptr =='z'|| *ptr =='f'|| *ptr =='g'||
+            *ptr =='h'|| *ptr =='j'|| *ptr =='k'|| *ptr =='l'|| *ptr =='m'|| *ptr =='n'||
+            *ptr =='p'|| *ptr =='q'|| *ptr =='r'|| *ptr =='s'|| *ptr =='t'|| *ptr =='v'|| 
+            *ptr =='w'|| *ptr =='x') consonnes++; // miniscules
+
+        if(*ptr =='b'-32|| *ptr =='c'-32|| *ptr =='d'-32|| *ptr =='z'-32|| *ptr =='f'-32|| *ptr =='g'-32||
+            *ptr =='h'-32|| *ptr =='j'-32|| *ptr =='k'-32|| *ptr =='l'-32|| *ptr =='m'-32|| *ptr =='n'-32||
+            *ptr =='p'-32|| *ptr =='q'-32|| *ptr =='r'-32|| *ptr =='s'-32|| *ptr =='t'-32|| *ptr =='v'-32|| 
+            *ptr =='w'-32|| *ptr =='x'-32) consonnes++; // majiscules
+
+        ptr++; 
     }
 
     // affichage du nombres de voyelles et consonnes
