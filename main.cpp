3bdbccf5b9133ef3062744fb5ad46f0ae90f9061
Kamdem-cmd <kamdemkevin87@gmail.com>
2025-01-28 23:52:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,9 +14,15 @@ int main(int argc, char ** argv[]){
     /*char* ptr = phrase;*/ 
     consonnes = compte_cons(phrase);
     voyelles = compte_voy(phrase);
+    spaces = compte_spaces(phrase);
+    mots = compte_mots(phrase);
+
 
     // affichage du nombres de voyelles et consonnes
-    std::cout << "\nconsonnes: " << consonnes <<"voyelles: " << voyelles <<std::endl;
+    std::cout <<"voyelles: " << voyelles << "\nconsonnes: " << consonnes << "\n";
+
+    // affichage du nombres de mots et espaces
+    std::cout <<"espaces: " << voyelles << "\nmots: " << consonnes << "\n";
 
     return 0;
 }
