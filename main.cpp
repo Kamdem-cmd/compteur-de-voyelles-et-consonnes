8b18782bcaed34ea69f69d6ab109e0a7047cfb99
Kamdem-cmd <kamdemkevin87@gmail.com>
2025-01-28 21:54:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "header/header.h"
+
 int main(int argc, char ** argv[]){
     int const taille = 250;
     char phrase[taille];
@@ -9,25 +11,9 @@ int main(int argc, char ** argv[]){
     std::cout << ">> " ;
     std::cin.getline(phrase, taille);
 
-    char* ptr = phrase;
-    while(*ptr != '\0'){
-        // incrementation du nombre de voyelles
-        if(*ptr =='a'|| *ptr =='e'|| *ptr =='i'|| *ptr =='o'|| *ptr =='u'|| *ptr =='y') voyelles++; // miniscules
-        if(*ptr =='a'-32|| *ptr =='e'-32|| *ptr =='i'-32|| *ptr =='o'-32|| *ptr =='u'-32|| *ptr =='y'-32)voyelles++; // majiscules
-
-        // incrementation du nombre de consonnes
-        if(*ptr =='b'|| *ptr =='c'|| *ptr =='d'|| *ptr =='z'|| *ptr =='f'|| *ptr =='g'||
-            *ptr =='h'|| *ptr =='j'|| *ptr =='k'|| *ptr =='l'|| *ptr =='m'|| *ptr =='n'||
-            *ptr =='p'|| *ptr =='q'|| *ptr =='r'|| *ptr =='s'|| *ptr =='t'|| *ptr =='v'|| 
-            *ptr =='w'|| *ptr =='x') consonnes++; // miniscules
-
-        if(*ptr =='b'-32|| *ptr =='c'-32|| *ptr =='d'-32|| *ptr =='z'-32|| *ptr =='f'-32|| *ptr =='g'-32||
-            *ptr =='h'-32|| *ptr =='j'-32|| *ptr =='k'-32|| *ptr =='l'-32|| *ptr =='m'-32|| *ptr =='n'-32||
-            *ptr =='p'-32|| *ptr =='q'-32|| *ptr =='r'-32|| *ptr =='s'-32|| *ptr =='t'-32|| *ptr =='v'-32|| 
-            *ptr =='w'-32|| *ptr =='x'-32) consonnes++; // majiscules
-
-        ptr++; 
-    }
+    /*char* ptr = phrase;*/ 
+    consonnes = compte_cons(phrase);
+    voyelles = compte_voy(phrase);
 
     // affichage du nombres de voyelles et consonnes
     std::cout <<"voyelles: " << voyelles << "\nconsonnes: " << consonnes;
