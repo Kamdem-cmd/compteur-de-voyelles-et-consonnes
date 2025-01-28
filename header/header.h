2c72800eff284bcb69b217cd1ee330e5edc33156
Kamdem-cmd <kamdemkevin87@gmail.com>
2025-01-28 19:49:45 +0100

    Update header.h
    mise à jours pour les fonctions pour voyelle et consonnes ...

--- header/header.h
+++ header/header.h
@@ -3,6 +3,8 @@
 
 #include <cstring>
 
+int compte_voy(const char *phrase);
+int compte_cons(const char *phrase);
 int compte_mots(const char *phrase);
 int compte_spaces(const char *phrase);
 int countSyllables(const char* word);
